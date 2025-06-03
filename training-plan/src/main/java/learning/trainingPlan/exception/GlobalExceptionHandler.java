f5e31d244b90c8f654e97fa8cf13a4b2cbc9805d
Sheep7291 <dominik02jankowski@wp.pl>
2025-06-03 22:53:03 +0200

    Add MethodArgumentNotValidException

--- training-plan/src/main/java/learning/trainingPlan/exception/GlobalExceptionHandler.java
+++ training-plan/src/main/java/learning/trainingPlan/exception/GlobalExceptionHandler.java
@@ -1,12 +1,18 @@
 package learning.trainingPlan.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
+import learning.trainingPlan.dto.ConstraintViolation;
 import learning.trainingPlan.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -25,6 +31,28 @@ public class GlobalExceptionHandler {
         );
         return ResponseEntity.status(HttpStatus.CONFLICT).body((errorResponse));
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception){
+    ProblemDetail validationProblemDetail =
+    ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Validation error");
+
+        List<ConstraintViolation> errors = exception.getFieldErrors()
+                .stream()
+                .map(violation -> ConstraintViolation.builder()
+                        .message(violation.getDefaultMessage())
+                        .fieldName(violation.getField())
+                        .rejectedValue(Objects.isNull(
+                                violation.getRejectedValue()) ?
+                                "null":
+                                violation.getRejectedValue().toString())
+                        .build())
+                .collect(Collectors.toList());
+        validationProblemDetail.setProperty("errors", errors);
+        return validationProblemDetail;
+    }
+
 
     @ExceptionHandler({ClientAlreadyHaveTrainerException.class})
     public ProblemDetail handleClientAlreadyHaveTrainerException(ClientAlreadyHaveTrainerException exception) {
